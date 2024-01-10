6820fcb2f1ee0eaadcacf379f25271cb66a2a158
Volodymyr Shafran <shafran.valdemarovich@gmail.com>
2024-01-10 18:15:27 +0200

    use seeds instead of custom script

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,13 @@
 # frozen_string_literal: true
+
 # This file should ensure the existence of records required to run the application in every environment (production,
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+
+raw_data = JSON.parse(File.read('db/data/data.json'))
+
+ProgrammingLanguage.transaction do
+  raw_data.each do |row|
+    ProgrammingLanguage.create!(name: row['Name'], type: row['Type'], designed_by: row['Designed by'])
+  end
+end
